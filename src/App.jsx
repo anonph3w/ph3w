4f58cae2da807a0b0729913efc9d5867d0a716fc
shrey <shrey.shreyashh@gmail.com>
2025-02-11 14:05:38 +0530

    pfp and socials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css'
 import Navbar from "./components/Navbar";
+import Hello from "./components/Hello";
 
 function App(){
     return(
         <>
             <div className="md:w-[55vw] m-auto w-[98vw]">
                 <Navbar></Navbar>
+                <Hello></Hello>
             </div>
         </>
     )
---
+++ src/components/Hello.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import asciiArt from '../assets/comic.png'
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+function Hello() {
+    return (
+        <>
+                <div className="|">
+                    <img className="md:w-[30%] m-auto w-[70%]" src={asciiArt} alt="whitespacecoding-pfp" />
+                    <div className="flex align-center justify-center gap-5 my-4">
+                        <a href="https://github.com/wscodingg">
+                            <FaGithub className="size-7" />
+                        </a>
+                        <a href="https://x.com/wscoding">
+                            <FaXTwitter className="size-7" />
+                        </a>
+                    </div>
+                    <hr className="m-2"/>
+                </div> 
+        </>
+    )
+}
+
+export default Hello
