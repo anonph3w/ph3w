c6f89f4c2c1e5322917b8e8faef52a4913f7ce28
shrey <whitespacecoding@gmail.com>
2025-02-18 16:15:52 +0530

    minor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <>
-            <p className="flex justify-between mx-4 font-mono pt-10 pb-5 md:text-base text-xs"><span >love you all ❤️</span> <p className="opacity-70">built with <a href="https://react.dev/" target="_blank" className="underline underline-offset-4 ">React</a></p></p>
+            <p className="flex justify-between mx-4 font-mono pt-10 pb-5 md:text-base text-xs"><span >love you all ❤️</span></p>
         </>
     )
 }
